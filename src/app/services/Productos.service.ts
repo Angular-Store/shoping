a4444ab7e088a0b2e86bd51514a443d87f11ca13
valemr05 <valeria.munoz3@utp.edu.co>
2023-07-16 13:58:09 -0500

    service

---
+++ src/app/services/Productos.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/enviroment/enviroment';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  private apiUrl = environment.api; // Obtén la URL de la API del objeto environment
+  constructor(private http: HttpClient) { }
+  getProductos() {
+    const url = `${this.apiUrl}/products`; // Construye la URL completa de la API
+    return this.http.get<any>(url); // Realiza la solicitud GET a la URL de la API y obtén los datos en formato JSON
+  }
+}
+
+// '/user/:userID/active'   
+// `${this.apiUrl}/:usersID`
+
+export class carProductsService {
+  private apiUrl = environment.api;
+  constructor(private http: HttpClient){}
+
+  showProducts(userID: string){
+    const url = `${this.apiUrl}/users/${userID}/active`
+    return this.http.get<any>(url);
+  }
+}
