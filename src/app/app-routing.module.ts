fcb7f259535029680f828e9f604bb7b9eb3a945f
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-07-16 12:49:30 -0500

    Products
    Con Material , Products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { HomeComponent } from './pages/Home/Home.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
-import { ProductsComponent } from './pages/products/products.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { CartComponent } from './pages/cart/cart.component';
 
@@ -12,7 +11,6 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'products', component: ProductsComponent},
   { path: 'details/:id', component: DetailsComponent},
   { path: 'cart', component: CartComponent},
 
@@ -21,6 +19,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-  
+
 })
 export class AppRoutingModule { }
