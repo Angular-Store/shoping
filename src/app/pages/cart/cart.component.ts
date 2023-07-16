e19d8b40bb9d8f731a20a951e9eb78f88b41e3e2
valemr05 <136719988+valemr05@users.noreply.github.com>
2023-07-16 13:19:42 -0500

    Revert "Carro con db"

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { carProductsService } from 'src/app/services/Productos.service';
-import { ActivatedRoute } from '@angular/router';
 
 interface Producto {
   nombre: string;
@@ -14,78 +12,60 @@ interface Producto {
 })
 
 export class CartComponent implements OnInit {
-  datos: any;
-  // productos: Producto[] = [
-  //   { nombre: 'Producto 1', precio: 10.99 },
-  //   { nombre: 'Producto 2', precio: 19.99 },
-  //   { nombre: 'Producto 3', precio: 5.49 }
-  // ];
+  productos: Producto[] = [
+    { nombre: 'Producto 1', precio: 10.99 },
+    { nombre: 'Producto 2', precio: 19.99 },
+    { nombre: 'Producto 3', precio: 5.49 }
+  ];
 
-  // totalSuma: number = 0;
+  totalSuma: number = 0;
 
-  constructor(private carProducts: carProductsService, private route: ActivatedRoute) { }
+  constructor() { }
 
-  // ngOnInit(): void {
-  //   this.agregarNuevoProducto();
-  //   this.calcularTotal();
-  //   console.log(this.productos)
-  // }
-  ngOnInit(): void{
-    const userID = this.route.snapshot.params['userID'];
-    console.log('ID de usuario:', userID);
-    
-    this.getProducts(userID);
-    
+  ngOnInit(): void {
+    this.agregarNuevoProducto();
+    this.calcularTotal();
+    console.log(this.productos)
   }
 
-  getProducts(userID: string){
-    this.carProducts.showProducts(userID).subscribe
-    ((response: any) => {
-        console.log('Datos enviados', response);
-        return `/user/${userID}/active`;
-    },
-    (error: any) => {
-      console.error('Error al enviar los datos', error);
-    });
+  agregarNuevoProducto() {
+    const nuevoProducto: Producto = {
+      nombre: 'Nuevo Producto',
+      precio: 25.99
+    };
+
+    this.productos.push(nuevoProducto);
   }
-  // agregarNuevoProducto() {
-  //   const nuevoProducto: Producto = {
-  //     nombre: 'Nuevo Producto',
-  //     precio: 25.99
-  //   };
 
-  //   this.productos.push(nuevoProducto);
-  // }
+  seleccionarProducto(producto: Producto) {
+    // Aquí puedes implementar la lógica para seleccionar el producto
+    // Puedes guardar el producto seleccionado en otra variable o hacer algo con él.
+  }
 
-  // seleccionarProducto(producto: Producto) {
-  //   // Aquí puedes implementar la lógica para seleccionar el producto
-  //   // Puedes guardar el producto seleccionado en otra variable o hacer algo con él.
-  // }
+  borrarProducto(producto: Producto) {
+    // Aquí puedes implementar la lógica para borrar el producto
+    const index = this.productos.indexOf(producto);
+    if (index !== -1) {
+      this.productos.splice(index, 1);
+      this.calcularTotal();
+    }
+  }
 
-  // borrarProducto(producto: Producto) {
-  //   // Aquí puedes implementar la lógica para borrar el producto
-  //   const index = this.productos.indexOf(producto);
-  //   if (index !== -1) {
-  //     this.productos.splice(index, 1);
-  //     this.calcularTotal();
-  //   }
-  // }
+  calcularTotal() {
+    this.totalSuma = this.productos.reduce((total, producto) => total + producto.precio * this.cantidadProductos, 0);
+  }
 
-  // calcularTotal() {
-  //   this.totalSuma = this.productos.reduce((total, producto) => total + producto.precio * this.cantidadProductos, 0);
-  // }
+  cantidadProductos: number = 0;
 
-  // cantidadProductos: number = 0;
+  restarProducto() {
+    if (this.cantidadProductos > 0) {
+      this.cantidadProductos--;
+    }
+  }
 
-  // restarProducto() {
-  //   if (this.cantidadProductos > 0) {
-  //     this.cantidadProductos--;
-  //   }
-  // }
-
-  // sumarProducto() {
-  //   this.cantidadProductos++;
-  // }
+  sumarProducto() {
+    this.cantidadProductos++;
+  }
 
 
 }
