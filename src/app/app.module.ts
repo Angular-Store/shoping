034877f65c3990586e3b22cdd6b314faca78a424
Rafaelurrea <rafael.urrea@utp.edu.co>
2023-07-16 01:55:16 -0500

    Maquetacion ProductView
    se realizo la maquetación en angular, y algunos estilos en css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import {DetailsComponent} from './pages/details/details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule  } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent // Agrega FooterComponent a las declaraciones
+    FooterComponent, // Agrega FooterComponent a las declaraciones
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  api = 'https://rickandmortyapi.com/api/character/2';
+  producto: number;
 
-  ngOnInit() {
+  constructor(private http: HttpClient ) {
+    this.producto = 1;
   }
 
+  ngOnInit(): void {
+  }
+
+  aumentarCompra() {
+    this.producto++;
+    console.log("cantidad en " + this.producto);
+  }
+
+  disminuirCompra() {
+    if (this.producto === 0) {
+      console.log("cantidad en 0");
+    } else {
+      this.producto--;
+      console.log("cantidad en " + this.producto);
+    }
+  }
+
+  addToCart() {
+    this.consumirAPI();
+    alert('producto añadido al carro');
+  }
+
+  async consumirAPI() {
+    try {
+      const data = await this.http.get<any>(this.api).toPromise();
+      // Procesa la respuesta de la API
+      console.log(data);
+    } catch (error) {
+      // Maneja los errores de la solicitud
+      console.error(error);
+    }
+  }
 }
