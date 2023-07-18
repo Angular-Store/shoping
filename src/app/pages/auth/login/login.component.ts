137c8f3dd6c68df669fc4121a37428c21e346c24
Johan Boshell <136541718+BoshellJohan@users.noreply.github.com>
2023-07-17 19:59:36 -0500

    Comentarios de Registro

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/enviroment/enviroment';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 
-//Creación del componente
+//Creación automática del componente
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
